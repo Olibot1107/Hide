d22b564e8a42abf4e5585e96be1a569e38dbbfa2
Olibot13 <ojdawson1107@gmail.com>
2025-06-09 14:12:22 +0100

    coolllll ahhh updateee

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
 self.addEventListener('install', event => {
   console.log('Service worker installed');
+  alert("Installed")
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   console.log('Service worker activated');
+  alert("Service worker activated")
 });
 
 self.addEventListener('fetch', event => {
